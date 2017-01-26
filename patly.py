94d258a04090f3c27d339d5ed778108128120844
Pat Viafore <patviafore@gmail.com>
2017-01-25 20:11:46 -0600

    Adding barebones hardcoded server

---
+++ patly.py
@@ -0,0 +1,43 @@
+"""
+A hard-coded webserver meant for PyTN BDD to the Bone talk
+"""
+from bottle import  get, post, redirect, run, static_file
+
+
+@get("/")
+def return_index():
+    """
+    return index.html from the static folder
+    """
+    return return_static_file("index.html")
+
+@get("/static/<path:path>")
+def return_static_file(path):
+    """
+    return a static file from the static folder
+    param path the path to the file
+    """
+    return static_file(path, root="static")
+
+@post("/createShortenedLink")
+def create_shortened_link():
+    """
+    create a shortened link from the user
+    """
+    return {"shortened_link": "patly.com/1"}
+
+@get("/<_identifier>")
+def redirect_to_page(_identifier):
+    """
+    redirect to the appropriate id
+    param id the id that we've registered
+    """
+    redirect("https://google.com")
+
+@get("/get-stats")
+def get_stats():
+    return {"https://google.com": 4,
+            "https://python.org" : 5}
+
+
+run(host="0.0.0.0", reloader=True)
