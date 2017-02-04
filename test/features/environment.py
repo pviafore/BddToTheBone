495b04c2474d40b0e96b9f243ca4428ddc30d584
Pat Viafore <patviafore@gmail.com>
2017-02-03 21:46:59 -0600

    Adding final step

--- test/features/environment.py
+++ test/features/environment.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from test.patly_homepage_driver import *
 
 def before_scenario(context, scenario):
-    context.browser = webdriver.Chrome()
-    context.browser.get("http://pat.ly:8080")
+    browser = webdriver.Chrome()
+    context.browser = PatlyHomepageDriver(browser)
+    context.browser.go_to_homepage()
     context.mapping = {}
 
 def after_scenario(context ,scenario):
--- test/features/steps/url_shortening_steps.py
+++ test/features/steps/url_shortening_steps.py
@@ -5,47 +5,38 @@ def step_impl(context, url):
 
 @given(u'we have shortened the url {url}')
 def step_impl(context, url):
-    context.browser.find_element_by_id("input").send_keys(url)
-    context.browser.find_element_by_id("get-short-link").click()
     context.url = url
-    context.shortened_url = context.browser.find_element_by_id("return-link").text
+    context.shortened_url = context.browser.shorten(url)
     context.mapping[context.url] = context.shortened_url
 
-@given(u'we navigate to that shortened url {num_times} times')
+@given(u'we navigate to that shortened url {num_times:d} times')
 def step_impl(context, num_times):
     # go to the url 
-    for _ in range(int(num_times)):
-        context.browser.get(context.shortened_url)
+    for _ in range(num_times):
+        context.browser.go_to_page(context.shortened_url)
     #reset back to the original page
-    context.browser.get("http://pat.ly:8080")
+    context.browser.go_to_homepage()
 
 @when(u'we shorten it through our service')
 def step_impl(context):
-    context.browser.find_element_by_id("input").send_keys(context.url)
-    context.browser.find_element_by_id("get-short-link").click()
-    context.shortened_url = context.browser.find_element_by_id("return-link").text
+    context.shortened_url = context.browser.shorten(context.url)
 
 @when(u'we navigate to that shortened URL')
 def step_impl(context):
-    context.browser.get(context.shortened_url)
+    context.browser.go_to_page(context.shortened_url)
 
 @when(u'we look at the stats on the homepage')
 def step_impl(context):
-    context.stats = {}
-    table = context.browser.find_element_by_id("stats")
-    for row in table.find_elements_by_tag_name("tr")[1:]:
-        cells = row.find_elements_by_tag_name("td")
-        context.stats[cells[0]] = cells[1]
-
+    context.stats = context.browser.get_stats()
 
 @then(u'we arrive at {url} as fast as possible')
 def step_impl(context, url):
-    assert url == context.browser.current_url
+    assert context.browser.is_at_page(url)
     #how do we test speed?
 
 @then(u'we should receive a shortened URL')
 def step_impl(context):
-    assert context.browser.find_element_by_id("return-link").text.startswith("http://pat.ly:8080")
+    assert context.shortened_url.startswith("http://pat.ly:8080")
 
 @then(u'we see the shortened URL for {url} has been visited {num_times} times')
 def step_impl(context, url, num_times):
